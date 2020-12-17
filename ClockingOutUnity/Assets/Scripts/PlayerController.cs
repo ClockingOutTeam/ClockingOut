3826e7536e95bb0bc7293e33adbebffbc6fe350e
Logan <WolfGuardLegion11@gmail.com>
2020-12-17 15:21:25 -0800

    Player Rotation

--- ClockingOutUnity/Assets/Scripts/PlayerController.cs
+++ ClockingOutUnity/Assets/Scripts/PlayerController.cs
@@ -21,12 +21,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            transform.Translate((input * speed) * Time.deltaTime);
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        transform.Translate((input * speed) * Time.deltaTime);
 
-            if(Input.GetKeyDown("W") || Input.GetKeyDown("A") || Input.GetKeyDown("S") || Input.GetKeyDown("D") || Input.GetKeyDown("space"))
-            {
+        Vector3 inputRotate = new Vector3(0.0f, Input.GetAxis("horizontalR"), 0.0f);
+        transform.Rotate((inputRotate * speed) * Time.deltaTime); 
+        
+        
+        if(Input.GetKeyDown("W") || Input.GetKeyDown("A") || Input.GetKeyDown("S") || Input.GetKeyDown("D") || Input.GetKeyDown("space"))
+        {
                 timer.timerIsRunning = true;
-            }
+        }
     }
 }
