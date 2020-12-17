b5f6fe8af105ec92c0b88ee6340687b012065a38
Logan <WolfGuardLegion11@gmail.com>
2020-12-17 12:41:09 -0800

    Added Player and Enemy Script

---
+++ ClockingOutUnity/Assets/Scripts/Enemies.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    private GameObject playerRef;
+
+    public float speed = 3;
+
+    private bool detectPlayer = false;
+    private float detectionRange = 15;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(detectPlayer && playerRef != null)
+        {
+            Vector3 distanceToPlayer = (playerRef.transform.position - transform.position).normalized;
+            speed = 5;
+
+            transform.forward = distanceToPlayer;
+            transform.position += distanceToPlayer * speed * Time.deltaTime;
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ ClockingOutUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float health = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (health < 0)
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            transform.Translate((input * speed) * Time.deltaTime);
+
+            if(Input.GetKeyDown("W") || Input.GetKeyDown("A") || Input.GetKeyDown("S") || Input.GetKeyDown("D") || Input.GetKeyDown("space"))
+            {
+
+            }
+        }
+    }
+}
