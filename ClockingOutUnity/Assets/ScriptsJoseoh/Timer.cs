76c2fc878db8a206000b593cb79dfc72c6f79bcc
Joseph Huskey <s209062@students.aie.edu>
2020-12-17 13:07:22 -0800

    Folders added onto main, scripts renamed to avoid merge

---
+++ ClockingOutUnity/Assets/ScriptsJoseoh/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    void Start()
+    {
+        //this boolean is set to true when the scene is loaded(if this script is on an object)
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        //this will run as long as the timerIsRunning boolean is true.
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                //subtracts time
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                //this displays to the console tha tthe time has run out.
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+}
