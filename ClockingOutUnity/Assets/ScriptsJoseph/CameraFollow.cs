5a83209beab70b3c1439ddc2e475355d91d777d6
Joseph Huskey <s209062@students.aie.edu>
2020-12-18 11:57:11 -0800

    Fixed camera rotation

--- ClockingOutUnity/Assets/ScriptsJoseph/CameraFollow.cs
+++ ClockingOutUnity/Assets/ScriptsJoseph/CameraFollow.cs
@@ -4,29 +4,41 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject player;        //Public variable to store a reference to the player game object
-    public float direction;
-
+    public Transform player;        //Public variable to store a reference to the player game object
     private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    [Range(0.01f, 1.0f)]
+    public float smoothness; //some game dev friendly variables so they can just adjust the camera rotation through inspector
+    public bool lookAtPlayer = true;
+    public bool rotateAroundPlayer = true; //some basic bools depending on how we want this to work
+    public float rotationSpeed = 5.0f;
 
     // Using this for initialization
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        offset = transform.position - player.position;
     }
-    //this will allow for camera movement
-    void Update() 
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("No cry");
-        }
-    }
+    
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
+        if (rotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+
+            //resets offset.
+            offset = camTurnAngle * offset;
+        }
+
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        Vector3 newPos = player.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothness);
+
+        if (lookAtPlayer || rotateAroundPlayer) 
+        {
+            transform.LookAt(player);
+        }
+      
     }
 }
