59f61cb150cdad89538e27125ca4ed6b2dcc5680
Logan <WolfGuardLegion11@gmail.com>
2020-12-17 17:18:54 -0800

    Player Start Game Object

--- ClockingOutUnity/Assets/Scripts/PlayerController.cs
+++ ClockingOutUnity/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject player;
     public float speed = 5;
-    public float health = 3;
     public float xPause;
     public float yPause;
     public float zPause;
@@ -25,7 +24,7 @@ public class PlayerController : MonoBehaviour
         transform.Translate((input * speed) * Time.deltaTime);
 
         Vector3 inputRotate = new Vector3(0.0f, Input.GetAxis("horizontalR"), 0.0f);
-        transform.Rotate((inputRotate * speed * 4) * Time.deltaTime);
+        transform.Rotate((inputRotate * speed * 10) * Time.deltaTime);
         
         Vector3 inputJump = new Vector3(0.0f, Input.GetAxis("Jump"), 0.0f);
         transform.Translate((inputJump * speed) * Time.deltaTime);
