b791fd83512abf3297f26dd35901f9499abbcaef
Joseph Huskey <s209062@students.aie.edu>
2020-12-18 16:05:05 -0800

    fixed jump and player movement

--- ClockingOutUnity/Assets/Scripts/PlayerController.cs
+++ ClockingOutUnity/Assets/Scripts/PlayerController.cs
@@ -5,45 +5,85 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject player;
     public Rigidbody rgb;
-    public float speed = 5;
+    //movement floats
+    public float speed = .1f; //speed of walking
+    public float jumpStrength = 0.05f;
+    public float rotationSpeed = 360;
+    //positions lol Logan misheard me, this is if we finish the game earlier then I expect
     public float xPause;
     public float yPause;
     public float zPause;
+
     public Timer timer;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        timer = player.GetComponent<Timer>();
-    }
+    //last minute hot fix trash variables
+    public int count;
+    public int maxJumpTime = 100;
+    //input axes
+    private string moveInputAxis = "Vertical"; //this one will only control moving the player
+    private string rotateInputAxis = "Horizontal"; //this one will only control rotation.
+    private string jumpInputAxis = "Jump";
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        transform.Translate((input * speed) * Time.deltaTime);
-
-        Vector3 inputRotate = new Vector3(0.0f, Input.GetAxis("horizontalR"), 0.0f);
-        transform.Rotate((inputRotate * speed * 10) * Time.deltaTime);
-        
-        Vector3 inputJump = new Vector3(0.0f, Input.GetAxis("Jump"), 0.0f);
-        transform.Translate((inputJump * speed) * Time.deltaTime);
-
-        if (Input.anyKey)
-        {
-                timer.timerIsRunning = true;
-        }
-
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float rotateAxis = Input.GetAxis(rotateInputAxis); //getting references to the unity axes
+        float jumpAxis = Input.GetAxis(jumpInputAxis);
+        ApplyInput(moveAxis, rotateAxis, jumpAxis); //this will update our position pretty easily, need to add jump but I want this working before I rewrite Logan's script
     }
 
+    private void ApplyInput(float moveInput, float turnInput, float jumpInput) //just creating methods for all of this so it is easier to read
+    {
+        Move(moveInput);
+        Turn(turnInput);
+        Jump(jumpInput);
+    }
+
+    //movement method
+    private void Move(float input)
+    {
+        transform.Translate(Vector3.forward*input*speed);
+    }
+
+    //rotate the player
+    private void Turn(float input) 
+    {
+        transform.Rotate(0, input * rotationSpeed * Time.deltaTime, 0);
+    }
+    //if you are reviewing the code I have written lets just skip over this mess.
+    private void Jump(float input) 
+    {
+        if (input > 0)
+        {
+            if (count<maxJumpTime)
+            {
+                transform.Translate(Vector3.up * (input * jumpStrength));
+                count++;
+            }
+        }else if (count >= maxJumpTime) 
+        {
+            count++;
+            if(count <= maxJumpTime*3)
+            {
+                count = 0;
+            }
+        }
+    }
+    //instant death method
     private void OnCollisionEnter(Collision other)
     {  
+        //death
         if (other.gameObject.tag == "Damage")
         {
             Debug.Log("Hit Me!!!");
             SceneManager.LoadScene("DeathScene");
+        }
+        //win
+        if(other.gameObject.tag == "Win") //win will probably be changed later depending on game designers
+        {
+            Debug.Log("I won 0-0");
+            SceneManager.LoadScene("WinScene");
         }
     }
 }
