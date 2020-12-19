90e04b009af01ac07b65b0a58594666147ae75cb
Joseph Huskey <s209062@students.aie.edu>
2020-12-18 17:05:58 -0800

    Added scenes/buttons

---
+++ ClockingOutUnity/Assets/ScriptsJoseph/ToMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMain : MonoBehaviour
+{
+    public void ToMainMenu() 
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
